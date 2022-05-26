923170cda54b6c78e0cf074944a9f4f2f8cd4331
suubinkim <0vbbbv0@gmail.com>
2022-05-27 00:04:28 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/yapp/betree/BeTreeApplication.java
@@ -0,0 +1,13 @@
+package com.yapp.betree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeTreeApplication.class, args);
+    }
+
+}
