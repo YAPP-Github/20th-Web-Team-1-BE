c2a5dc319896920e23d65192ec295876f8e90d87
suubinkim <0vbbbv0@gmail.com>
2022-06-01 18:47:37 +0900

    feat: Swagger 의존성 추가 및 설정 파일 등록

---
+++ src/main/java/com/yapp/betree/config/swagger/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.yapp.betree.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    /**
+     * 스웨거 API 문서 생성
+     * @return Docket
+     */
+    @Bean
+    public Docket swggerAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .forCodeGeneration(true)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .enable(true)
+                .useDefaultResponseMessages(true);
+    }
+
+    /**
+     * 스웨거 정보
+     * @return ApiInfo
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("BeTree API 문서")
+                .description("YAPP20 Web1-BeTree API 문서입니다")
+                .version("1.0")
+                .build();
+    }
+
+}
