53fed7b5542d40044d8bd4f2eb5d68fb3253daab
kang-jisu <rkdwltn0425@gmail.com>
2022-06-05 00:21:22 +0900

    feat: 레파지토리 생성

---
+++ src/main/java/com/yapp/betree/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.yapp.betree.repository;
+
+import com.yapp.betree.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
