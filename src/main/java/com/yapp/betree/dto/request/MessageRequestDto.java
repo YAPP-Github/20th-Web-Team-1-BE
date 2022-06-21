f974296aeb5c9b6d6075984a9498f5e31f9836bc
kang-jisu <rkdwltn0425@gmail.com>
2022-06-22 03:38:01 +0900

    refactor: 물주기 api 문서 작성, 예외 처리
    - 테스트코드 작성 필요

--- src/main/java/com/yapp/betree/dto/request/MessageRequestDto.java
+++ src/main/java/com/yapp/betree/dto/request/MessageRequestDto.java
@@ -1,17 +1,35 @@
 package com.yapp.betree.dto.request;
 
+import io.swagger.annotations.ApiModel;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Getter
+@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ApiModel(description = "물주기 요청 DTO")
 public class MessageRequestDto {
 
+    @NotNull(message = "받는사람 id를 입력해주세요.")
+    @Positive(message = "올바른 id를 입력해주세요.")
     private Long receiverId;
+
+    @NotBlank(message = "메시지를 입력해주세요.")
+    @Length(min = 10, max = 1000, message = "메시지는 최소 10자, 최대 1000자 입력해야합니다.")
     private String content;
+
+    @NotNull(message = "폴더 id를 입력해주세요.")
+    @Positive(message = "올바른 id를 입력해주세요.")
     private Long folderId;
+
     private boolean anonymous;
 
     @Builder
--- src/main/java/com/yapp/betree/dto/request/TreeRequestDto.java
+++ src/main/java/com/yapp/betree/dto/request/TreeRequestDto.java
@@ -1,6 +1,7 @@
 package com.yapp.betree.dto.request;
 
 import com.yapp.betree.domain.FruitType;
+import io.swagger.annotations.ApiModel;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,11 +11,11 @@ import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ApiModel(description = "나무추가 요청 DTO")
 public class TreeRequestDto {
 
     @NotBlank(message = "나무 이름은 빈값일 수 없습니다.")
     private String name;
-
 
     private FruitType fruitType;
 
