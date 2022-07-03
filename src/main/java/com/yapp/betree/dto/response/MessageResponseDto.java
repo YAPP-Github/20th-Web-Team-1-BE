1339d6de5a4c3be57ba88317d97518b4722e9ce3
kang-jisu <rkdwltn0425@gmail.com>
2022-07-04 00:17:37 +0900

    fix: MessageResponseDto hashCode, equals 오버라이딩

--- src/main/java/com/yapp/betree/dto/response/MessageResponseDto.java
+++ src/main/java/com/yapp/betree/dto/response/MessageResponseDto.java
@@ -2,13 +2,16 @@ package com.yapp.betree.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.yapp.betree.domain.Message;
-import com.yapp.betree.domain.User;
 import com.yapp.betree.dto.SendUserDto;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.util.Objects;
+
+@ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -35,5 +38,31 @@ public class MessageResponseDto {
                 .senderNickname(message.isAnonymous() ? "익명" : user.getNickname())
                 .senderProfileImage(message.isAnonymous() ? "기본 이미지" : user.getUserImage())
                 .build();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content, anonymous, senderNickname, senderProfileImage);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        MessageResponseDto objDto = (MessageResponseDto) obj;
+        if (this.id != objDto.getId()) {
+            return false;
+        }
+        if (!this.content.equals(objDto.getContent())) {
+            return false;
+        }
+        if (this.anonymous != objDto.isAnonymous()) {
+            return false;
+        }
+        if (!this.senderNickname.equals(objDto.getSenderNickname())) {
+            return false;
+        }
+        if (!this.senderProfileImage.equals(objDto.getSenderProfileImage())) {
+            return false;
+        }
+        return true;
     }
 }
