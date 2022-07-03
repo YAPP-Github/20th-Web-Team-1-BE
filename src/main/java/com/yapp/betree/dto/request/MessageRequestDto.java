b63e65cff1aca49c7b7db9ab58cd382e0e045e47
kang-jisu <rkdwltn0425@gmail.com>
2022-07-03 22:06:30 +0900

    docs: 물주기 요청 DTO 설명추가

--- src/main/java/com/yapp/betree/dto/request/MessageRequestDto.java
+++ src/main/java/com/yapp/betree/dto/request/MessageRequestDto.java
@@ -1,6 +1,7 @@
 package com.yapp.betree.dto.request;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,6 +21,7 @@ public class MessageRequestDto {
 
     @NotNull(message = "받는사람 id를 입력해주세요.")
     @Positive(message = "올바른 id를 입력해주세요.")
+    @ApiModelProperty(value = "칭찬 메시지를 보낼 나무숲 주인 유저 아이디(받는사람)")
     private Long receiverId;
 
     @NotBlank(message = "메시지를 입력해주세요.")
@@ -27,6 +29,7 @@ public class MessageRequestDto {
     private String content;
 
     @Positive(message = "올바른 id를 입력해주세요.")
+    @ApiModelProperty(value = "칭찬 메시지를 보낼 지정 폴더, 자신에게 보낼때만 id를 명시하고 다른사람에게 보낼 때는 null로 입력")
     private Long folderId;
 
     private boolean anonymous;
