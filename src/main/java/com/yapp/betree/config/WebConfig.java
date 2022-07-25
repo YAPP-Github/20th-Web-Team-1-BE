1f7d6c0df9a83916ae755f79c9f33d0b60e9c98c
강지수 <37580216+kang-jisu@users.noreply.github.com>
2022-07-26 02:34:47 +0900

    fix: update WebConfig

--- src/main/java/com/yapp/betree/config/WebConfig.java
+++ src/main/java/com/yapp/betree/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowCredentials(true)
